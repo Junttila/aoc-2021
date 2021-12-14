e2fecdaad9fa9cbb768f5508a5654e3fba1f4054
Joakim Junttila <joakim@juntti.la>
2021-12-14 08:28:09 +0100

    Add timing of execution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import solutions from './solutions/day8/solutions';
+import solutions from './solutions/day14/solutions';
 
 solutions.forEach((f, i) => {
+  console.log('Start');
+  const before = new Date();
   const result = f();
-  console.log('Solution', (i + 1).toString());
+  const after = new Date();
+  console.log(
+    'Solution',
+    (i + 1).toString(),
+    `(${after.valueOf() - before.valueOf()}ms)`,
+  );
   console.log(result || '');
   console.log();
 });
