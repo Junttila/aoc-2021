7ee13b856553d2870a015a36f866a8ec5fc0ee6c
Joakim Junttila <joakim@juntti.la>
2021-12-22 02:25:50 +0100

    Day 11

--- src/solutions/day11/solutions.ts
+++ src/solutions/day11/solutions.ts
@@ -1,14 +1,155 @@
 import { fileToArray } from '../scripts/fileToArray';
 
-const filePath = 'src/solutions/day2/input.txt';
+const filePath = 'src/solutions/day11/input.txt';
 const solutions: Array<() => string> = [
   // Solution part 1
   () => {
-    return '';
+    let octoMap = fileToArray(filePath).map((v) => v.split('').map(Number));
+    // console.log(octoMap.join('\n'));
+
+    const height = octoMap.length;
+    const width = octoMap[0].length;
+
+    let flashCount = 0;
+
+    let steps = 100;
+
+    while (steps > 0) {
+      const newMap = new Array(...octoMap).map((v) => v.map((u) => u + 1));
+      const flashes = newMap.map((v) => v.map((_) => false));
+      let recentFlash = false;
+
+      do {
+        recentFlash = false;
+        for (let y = 0; y < height; y++) {
+          for (let x = 0; x < width; x++) {
+            if (!flashes[y][x] && newMap[y][x] > 9) {
+              // console.log(x, y, 'flashes');
+              flashes[y][x] = true;
+              recentFlash = true;
+              flashCount++;
+
+              if (y - 1 >= 0) {
+                newMap[y - 1][x]++;
+                if (x - 1 >= 0) {
+                  newMap[y - 1][x - 1]++;
+                }
+                if (x + 1 < width) {
+                  newMap[y - 1][x + 1]++;
+                }
+              }
+              if (y + 1 < height) {
+                newMap[y + 1][x]++;
+                if (x + 1 < width) {
+                  newMap[y + 1][x + 1]++;
+                }
+                if (x - 1 >= 0) {
+                  newMap[y + 1][x - 1]++;
+                }
+              }
+              if (x - 1 >= 0) {
+                newMap[y][x - 1]++;
+              }
+              if (x + 1 < width) {
+                newMap[y][x + 1]++;
+              }
+            }
+          }
+        }
+      } while (recentFlash);
+
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          if (flashes[y][x]) {
+            newMap[y][x] = 0;
+          }
+        }
+      }
+
+      // console.log();
+      // console.log(newMap.join('\n'));
+      octoMap = new Array(...newMap);
+
+      steps--;
+    }
+
+    console.log('flashes:', flashCount);
+
+    return flashCount.toString();
   },
   // Solution part 2
   () => {
-    return '';
+    let octoMap = fileToArray(filePath).map((v) => v.split('').map(Number));
+
+    const height = octoMap.length;
+    const width = octoMap[0].length;
+
+    let sync = false;
+    let stepsToSync = 0;
+
+    while (!sync) {
+      const newMap = new Array(...octoMap).map((v) => v.map((u) => u + 1));
+      const flashes = newMap.map((v) => v.map((_) => false));
+      let recentFlash = false;
+
+      do {
+        recentFlash = false;
+        for (let y = 0; y < height; y++) {
+          for (let x = 0; x < width; x++) {
+            if (!flashes[y][x] && newMap[y][x] > 9) {
+              // console.log(x, y, 'flashes');
+              flashes[y][x] = true;
+              recentFlash = true;
+
+              if (y - 1 >= 0) {
+                newMap[y - 1][x]++;
+                if (x - 1 >= 0) {
+                  newMap[y - 1][x - 1]++;
+                }
+                if (x + 1 < width) {
+                  newMap[y - 1][x + 1]++;
+                }
+              }
+              if (y + 1 < height) {
+                newMap[y + 1][x]++;
+                if (x + 1 < width) {
+                  newMap[y + 1][x + 1]++;
+                }
+                if (x - 1 >= 0) {
+                  newMap[y + 1][x - 1]++;
+                }
+              }
+              if (x - 1 >= 0) {
+                newMap[y][x - 1]++;
+              }
+              if (x + 1 < width) {
+                newMap[y][x + 1]++;
+              }
+            }
+          }
+        }
+      } while (recentFlash);
+
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          if (flashes[y][x]) {
+            newMap[y][x] = 0;
+          }
+        }
+      }
+
+      sync = flashes.flat().every((v) => v);
+
+      stepsToSync++;
+
+      // console.log('sync:', sync);
+      // console.log(newMap.join('\n'));
+      octoMap = new Array(...newMap);
+    }
+
+    console.log('steps to sync:', stepsToSync);
+
+    return stepsToSync.toString();
   },
 ];
 
