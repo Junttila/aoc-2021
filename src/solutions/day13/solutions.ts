7f87c93645628e05a112f1da26cc6e1fdc2dc8d1
Joakim Junttila <joakim@juntti.la>
2022-03-31 10:00:03 +0200

    Day 13 WIP

--- src/solutions/day13/solutions.ts
+++ src/solutions/day13/solutions.ts
@@ -1,9 +1,24 @@
 import { fileToArray } from '../scripts/fileToArray';
 
-const filePath = 'src/solutions/day2/input.txt';
+const filePath = 'src/solutions/day13/example.txt';
 const solutions: Array<() => string> = [
   // Solution part 1
   () => {
+    const array = fileToArray(filePath);
+
+    const splitPoint = array.indexOf('');
+
+    const dots = array
+      .slice(0, splitPoint)
+      .map((s) => s.split(',').map(Number));
+    const folds = array.slice(splitPoint + 1).map((s) => {
+      const [along, index] = s.slice(s.length - 3).split('=');
+      return { along, index };
+    });
+
+    console.log(dots, folds);
+    console.log(paperToString(dots, [11, 15]));
+
     return '';
   },
   // Solution part 2
@@ -11,5 +26,17 @@ const solutions: Array<() => string> = [
     return '';
   },
 ];
+
+function paperToString(points: number[][], [x, y]: number[]) {
+  const paper = new Array(y).fill('').map(() => {
+    return new Array<string>(x).fill('.');
+  });
+
+  points.forEach(([x, y]) => {
+    paper[y][x] = '#';
+  });
+
+  return paper.map((v) => v.join('')).join('\n');
+}
 
 export default solutions;
